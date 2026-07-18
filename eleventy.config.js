2b783a22a76616843ae357ca97a20ad6cf788201
colereichenberg <cole.reichenberg@gmail.com>
2026-07-18 14:43:47 -0600

    fonts, logo, shortcodes oh my!

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,10 @@ export default function (eleventyConfig) {
 	// Forces Eleventy to physically copy files during --serve
 	eleventyConfig.setServerPassthroughCopyBehavior("copy");
 	//eleventyConfig.addWatchTarget("src/assets/css/");
+
+	eleventyConfig.addShortcode("button", function(buttonText){
+		return `<button class="px-6 m-2 h-10 rounded-xl bg-green-700 border-none text-slate-200 font-semibold focus:text-slate-200 hover:text-slate-200 transition delay-50 duration-150 ease-in hover:scale-105 hover:bg-green-800 ">${buttonText}</button>`;
+	})
 }
 
 export const config = {
