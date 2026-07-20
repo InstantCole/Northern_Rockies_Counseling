e306606bb4e180c4bc5632ba0e895cd0c2fa96dd
colereichenberg <cole.reichenberg@gmail.com>
2026-07-20 00:36:50 -0600

    More work

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,21 @@
 export default function (eleventyConfig) {
+	eleventyConfig.setBrowserSyncConfig({
+		online: false
+	  });
 	eleventyConfig.setInputDirectory('src');
 	eleventyConfig.setOutputDirectory('dist');
 	// Set directories to pass through to the dist folder
 	eleventyConfig.addPassthroughCopy('src/assets/images');
 	eleventyConfig.addPassthroughCopy('src/assets/logos');
+	eleventyConfig.addPassthroughCopy({
+		'./node_modules/alpinejs/dist/cdn.js': './assets/js/alpine.js',
+	  })
 	// Forces Eleventy to physically copy files during --serve
 	eleventyConfig.setServerPassthroughCopyBehavior("copy");
 	//eleventyConfig.addWatchTarget("src/assets/css/");
 
 	eleventyConfig.addShortcode("button", function(buttonText){
-		return `<button class="px-6 m-2 h-10 rounded-xl bg-green-700 border-none text-slate-200 font-semibold focus:text-slate-200 hover:text-slate-200 transition delay-50 duration-150 ease-in hover:scale-105 hover:bg-green-800 ">${buttonText}</button>`;
+		return `<button class="px-6 py-2 m-2 min-h-48px rounded-xl bg-green-700 border-none text-slate-200 font-semibold focus:text-slate-200 hover:text-slate-200 transition delay-50 duration-150 ease-in hover:scale-105 hover:bg-green-800 ">${buttonText}</button>`;
 	})
 }
 
